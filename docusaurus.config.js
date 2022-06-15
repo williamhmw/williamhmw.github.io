d4d5b92af9141b43496a067c837245e1fb75baea
william <william.hm.w@hotmail.com>
2022-06-15 19:44:44 -0300

    deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'williamhmw', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'williamhmw.github.io', // Usually your repo name.
   deploymentBranch: "gh-pages",
 
   // Even if you don't use internalization, you can use this field to set useful
